c8cc85b3f640bb1dee43ac8e0b6ee966704c741d
Andrey Monastyrev <andreyreseed@gmail.com>
2024-05-02 18:30:49 +0500

    chore: add rules for eslint & add 'eslint.config..js' , 'tsconfig.eslint.json' files

--- ecommerce-application/.eslintrc.cjs
+++ ecommerce-application/.eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
@@ -12,16 +13,68 @@ module.exports = {
     'airbnb-typescript',
     'plugin:prettier/recommended',
   ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+    },
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true,
+    },
     project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'unused-imports', 'simple-import-sort', 'prettier', 'import'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-var': 'error',
+    'no-multi-spaces': 'error',
+    'space-in-parens': 'error',
+    'no-multiple-empty-lines': 'error',
+    'prefer-const': 'error',
+    'no-use-before-define': 'error',
+    'no-console': 'off',
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'max-line-per-function': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'unused-imports/no-unused-imports': 1,
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-var-requires': 2,
+    'implicit-arrow-linebreak': ['error', 'below'],
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        overrides: {
+          accessors: 'explicit',
+          constructors: 'no-public',
+          properties: 'explicit',
+          methods: 'explicit',
+          parameterProperties: 'explicit',
+        },
+      },
+    ],
   },
 };
