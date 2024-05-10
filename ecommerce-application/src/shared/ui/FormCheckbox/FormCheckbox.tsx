29933cd5bdc6a8f8bbe0f1a53f1f2e4e4b036a0f
Andrey Monastyrev <andreyreseed@gmail.com>
2024-05-10 12:58:21 +0500

    fix: delete eslint rule's disabling

--- ecommerce-application/src/shared/ui/FormCheckbox/FormCheckbox.tsx
+++ ecommerce-application/src/shared/ui/FormCheckbox/FormCheckbox.tsx
@@ -8,7 +8,6 @@ type FormCheckboxProps = Omit<FormControlLabelProps, 'control'>;
 export default function FormCheckbox({ style, ...props }: FormCheckboxProps): JSX.Element {
   return (
     <FormControlLabel
-      // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
       className={styles.FormCheckbox}
       control={<Checkbox sx={style} />}
