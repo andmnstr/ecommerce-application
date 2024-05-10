0c39e50805a64d587021a3023f5f3d7cca80a4d4
Baltika-Cat <134006853+Baltika-Cat@users.noreply.github.com>
2024-05-10 19:18:07 +0400

    Feat/RSS_ECOMM_UI_2: Link Component Implementation (#69)
    * feat: add custom link component to directory 'shared/UI'
    
    * feat: add styles for custom link component

---
+++ ecommerce-application/src/shared/UI/CustomLink/CustomLink.styles.ts
@@ -0,0 +1,10 @@
+export const customLinkStyle = {
+  '&.MuiLink-root': {
+    '&:hover': {
+      transform: 'scale(1.05)',
+      transition: '.5s',
+    },
+    display: 'inline-block',
+    transition: '.5s',
+  },
+};
