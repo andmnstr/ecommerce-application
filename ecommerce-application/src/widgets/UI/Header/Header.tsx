6c6182d1a7a5fe14d2044d93021788e4373743a2
Andrey Monastyrev <andreyreseed@gmail.com>
2024-05-11 18:05:36 +0300

    refactor: separate a 'navbar' component at it's folder

--- ecommerce-application/src/widgets/UI/Header/Header.tsx
+++ ecommerce-application/src/widgets/UI/Header/Header.tsx
@@ -1,56 +1,18 @@
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { AppBar, StyledEngineProvider, Toolbar } from '@mui/material';
+import { AppBar, StyledEngineProvider } from '@mui/material';
 import type * as React from 'react';
 
-import { CustomLink } from '../../../shared/UI/CustomLink/CustomLink';
 import { AuthSection } from './AuthSection';
 import styles from './Header.module.scss';
 import { Logo } from './Logo';
+import { Navbar } from './Navbar';
 
-const shopLinkStyle = {
-  display: 'flex',
-  alignItems: 'center',
-};
-
-export const Header: React.FC = () => {
+export const Header: React.FC = (): JSX.Element => {
   return (
     <StyledEngineProvider injectFirst>
       <AppBar className={styles.AppBar}>
-        <Toolbar className={styles.ToolBar}>
-          <Logo />
-          <div className={styles.NavBar}>
-            <CustomLink
-              href="/"
-              className={styles.Link}
-              sx={shopLinkStyle}
-            >
-              Home
-            </CustomLink>
-            <CustomLink
-              href=""
-              className={styles.Link}
-              sx={shopLinkStyle}
-            >
-              Shop
-              <KeyboardArrowDownIcon />
-            </CustomLink>
-            <CustomLink
-              href="/about"
-              className={styles.Link}
-              sx={shopLinkStyle}
-            >
-              About
-            </CustomLink>
-            <CustomLink
-              href="/contact"
-              className={styles.Link}
-              sx={shopLinkStyle}
-            >
-              Contact us
-            </CustomLink>
-          </div>
-          <AuthSection />
-        </Toolbar>
+        <Logo />
+        <Navbar />
+        <AuthSection />
       </AppBar>
     </StyledEngineProvider>
   );
---
+++ ecommerce-application/src/widgets/UI/Header/Navbar/config.ts
@@ -0,0 +1,6 @@
+export const items = [
+  { id: 1, text: 'Home', href: '/' },
+  { id: 2, text: 'Shop', href: '/shop' },
+  { id: 3, text: 'About', href: '/about' },
+  { id: 4, text: 'Contact us', href: '/contacts' },
+];
