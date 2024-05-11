5fcdf89f6b13268aa6b4fe63cf544a5a5f0eaf64
Andrey Monastyrev <andreyreseed@gmail.com>
2024-05-11 23:33:28 +0500

    refactor: add link components to 'login-signup' buttons for redirection to this pages

--- ecommerce-application/src/widgets/UI/Header/AuthSection/AuthSection.tsx
+++ ecommerce-application/src/widgets/UI/Header/AuthSection/AuthSection.tsx
@@ -1,23 +1,28 @@
 import type * as React from 'react';
 
 import { CustomButton } from '../../../../shared/ui/button/CustomButton';
+import { CustomLink } from '../../../../shared/ui/CustomLink/CustomLink';
 import styles from './AuthSection.module.scss';
+import { authPages } from './config';
 
 export const AuthSection: React.FC = (): JSX.Element => {
   return (
     <div className={styles.ButtonSection}>
-      <CustomButton
-        className={styles.Button}
-        variant="contained"
-      >
-        Login
-      </CustomButton>
-      <CustomButton
-        className={styles.Button}
-        variant="contained"
-      >
-        Signup
-      </CustomButton>
+      {authPages.map(page => {
+        return (
+          <CustomLink
+            href={page.href}
+            key={page.id}
+          >
+            <CustomButton
+              className={styles.Button}
+              variant="contained"
+            >
+              {page.text}
+            </CustomButton>
+          </CustomLink>
+        );
+      })}
     </div>
   );
 };
---
+++ ecommerce-application/src/widgets/UI/Header/AuthSection/config.ts
@@ -0,0 +1,4 @@
+export const authPages = [
+  { id: 5, text: 'Login', href: '/login' },
+  { id: 6, text: 'Signup', href: '/signup' },
+];
