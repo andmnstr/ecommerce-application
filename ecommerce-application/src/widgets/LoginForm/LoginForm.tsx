1acdfc84b38a467f7032a7731edd44de3d2cff30
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-05-11 16:34:56 +0300

    feat: implement show/hide password feature

--- ecommerce-application/src/widgets/LoginForm/LoginForm.tsx
+++ ecommerce-application/src/widgets/LoginForm/LoginForm.tsx
@@ -1,36 +1,78 @@
-import { Box } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Box, IconButton, InputAdornment, Typography } from '@mui/material';
 import type React from 'react';
+import { useState } from 'react';
 
 import { CustomButton } from '../../shared/UI/button/CustomButton';
 import CustomInputText from '../../shared/UI/CustomInputText/CustomInputText';
 import FormCheckbox from '../../shared/UI/FormCheckbox/FormCheckbox';
+import classes from './LoginForm.module.scss';
 
-interface LoginFormProps {
-  onSubmit: (e: React.FormEvent) => void;
-}
+export const LoginForm: React.FC = () => {
+  const [values, setValues] = useState({
+    password: '',
+    showPassword: false,
+  });
 
-export const LoginForm: React.FC<LoginFormProps> = ({ onSubmit, ...props }) => {
+  const showOrHidePassword = (): void => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
   return (
     <Box
+      className={classes.loginForm}
       component="form"
-      onSubmit={onSubmit}
-      {...props}
+      onSubmit={e => {
+        e.preventDefault();
+      }}
     >
-      <CustomInputText
-        type="email"
-        label="Enter your e-mail"
-      />
-      <CustomInputText label="Enter password" />
+      <div>
+        <Typography className={classes.loginTitle}>Welcome</Typography>
+        <Typography className={classes.loginDescription}>Please login here</Typography>
+      </div>
+      <div>
+        <CustomInputText
+          className={classes.loginInput}
+          type="e-mail"
+          label="Enter your e-mail"
+          fullWidth
+        />
+      </div>
+      <div>
+        <CustomInputText
+          className={classes.loginInput}
+          type={values.showPassword ? 'text' : 'password'}
+          label="Enter password"
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={showOrHidePassword}>
+                  {values.showPassword ? (
+                    <VisibilityOff style={{ color: '#131118' }} />
+                  ) : (
+                    <Visibility style={{ color: '#131118' }} />
+                  )}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </div>
       <FormCheckbox
+        className={classes.loginCheckbox}
         label="Remember Me"
-        // style={{ color: 'black', '&.Mui-checked': { color: 'black' } }}
       />
       <CustomButton
+        className={classes.loginButton}
         type="submit"
         variant="contained"
         fullWidth
       >
-        Log in
+        Login
       </CustomButton>
     </Box>
   );
