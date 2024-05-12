9828f521fb66f0dff2aa4d11a93e5bc8e72c5e1a
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-05-13 00:36:48 +0300

    refactor: move checking first space before checking length

--- ecommerce-application/src/widgets/LoginForm/Lib/schema.ts
+++ ecommerce-application/src/widgets/LoginForm/Lib/schema.ts
@@ -6,8 +6,8 @@ export const schema = yup.object().shape({
   password: yup
     .string()
     .required('Field is required')
-    .min(8, 'Password must be at least 8 characters long')
     .matches(/^[^\s]/, 'Password cannot start with a space')
+    .min(8, 'Password must be at least 8 characters long')
     .matches(/\d/, 'Password must contain at least one digit')
     .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
     .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
