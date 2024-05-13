6d4e4efc6628df2f277e84d8d9a2fe062c5810de
adamanta1 <elena.brashkina2020@gmail.com>
2024-05-13 05:05:22 +0400

    feat: create InputWithController file for inputs validation

---
+++ ecommerce-application/src/widgets/RegistrationForm/components/InputWithController/InputWithController.tsx
@@ -0,0 +1,36 @@
+import type React from 'react';
+import type { Control } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
+
+import CustomInputText from '../../../../shared/UI/CustomInputText/CustomInputText';
+import type { IRegistrationFields } from '../../lib/RegistrationFieldsInterface';
+
+interface IInputProps {
+  name: 'firstName' | 'lastName' | 'email' | 'password';
+  control: Control<IRegistrationFields>;
+  label: string;
+  helperText: string | undefined;
+}
+
+export const InputWithController: React.FC<IInputProps> = props => {
+  const { name, control, label, helperText } = props;
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => {
+        return (
+          <CustomInputText
+            onChange={field.onChange}
+            type={name === 'password' ? 'password' : 'text'}
+            label={label}
+            fullWidth
+            autoComplete="false"
+            helperText={helperText ?? ' '}
+            error={typeof helperText === 'string'}
+          />
+        );
+      }}
+    />
+  );
+};
