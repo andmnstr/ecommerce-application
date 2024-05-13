7808021f8ef5f97b948a1bb9cf381da88fc82d1f
Baltika-Cat <134006853+Baltika-Cat@users.noreply.github.com>
2024-05-13 06:46:47 +0400

    Feat: RSS_ECOMM_2_07. Login Form Validation (#80)
    * feat: add yup schema for login form validation
    
    * feat: add interface with login form's input fields
    
    * feat: add validation, show validation error messages on input value change
    
    * refactor: move checking first space before checking length
    
    * feat: add style for error messages

--- ecommerce-application/src/widgets/LoginForm/Components/LoginForm.tsx
+++ ecommerce-application/src/widgets/LoginForm/Components/LoginForm.tsx
@@ -1,13 +1,31 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Box, Typography } from '@mui/material';
 import type React from 'react';
 import { useState } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 
 import { CustomButton } from '../../../shared/UI/button/CustomButton';
 import CustomInputText from '../../../shared/UI/CustomInputText/CustomInputText';
+import { schema } from '../Lib/schema';
+import type { ILoginFormFields } from '../Lib/type';
 import classes from './LoginForm.module.scss';
 import { PasswordEndAdornment } from './PasswordEndAdornment';
 
 export const LoginForm: React.FC = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<ILoginFormFields>({
+    resolver: yupResolver(schema),
+    mode: 'onChange',
+  });
+
+  const submitForm: SubmitHandler<ILoginFormFields> = data => {
+    console.log({ data });
+  };
+
   const [values, setValues] = useState({
     password: '',
     showPassword: false,
@@ -24,38 +42,57 @@ export const LoginForm: React.FC = () => {
     <Box
       className={classes.loginForm}
       component="form"
-      onSubmit={e => {
-        e.preventDefault();
-      }}
-      autoComplete="false"
+      onSubmit={handleSubmit(submitForm)}
     >
       <div className={classes.loginTitleAndDescription}>
         <Typography className={classes.loginTitle}>Welcome</Typography>
         <Typography className={classes.loginDescription}>Please login here</Typography>
       </div>
       <div>
-        <CustomInputText
-          className={classes.loginInput}
-          type="e-mail"
-          label="Enter your e-mail"
-          fullWidth
-        />
-      </div>
-      <div>
-        <CustomInputText
-          className={classes.loginInput}
-          type={values.showPassword ? 'text' : 'password'}
-          label="Enter password"
-          fullWidth
-          InputProps={{
-            endAdornment: (
-              <PasswordEndAdornment
-                onClick={showOrHidePassword}
-                showPassword={values.showPassword}
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => {
+            return (
+              <CustomInputText
+                onChange={field.onChange}
+                className={classes.loginInput}
+                type="text"
+                label="Enter your e-mail"
+                fullWidth
+                autoComplete="false"
               />
-            ),
+            );
           }}
         />
+        <p className={classes.errorMessage}>{errors.email?.message}</p>
+      </div>
+      <div>
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => {
+            return (
+              <CustomInputText
+                onChange={field.onChange}
+                className={classes.loginInput}
+                type={values.showPassword ? 'text' : 'password'}
+                label="Enter password"
+                fullWidth
+                autoComplete="false"
+                InputProps={{
+                  endAdornment: (
+                    <PasswordEndAdornment
+                      onClick={showOrHidePassword}
+                      showPassword={values.showPassword}
+                    />
+                  ),
+                }}
+              />
+            );
+          }}
+        />
+        <p className={classes.errorMessage}>{errors.password?.message}</p>
       </div>
       <CustomButton
         className={classes.loginButton}
