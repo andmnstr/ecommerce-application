4d071a3da7650d68bdc03e4915c03539ccfb310a
adamanta1 <elena.brashkina2020@gmail.com>
2024-05-15 02:28:11 +0400

    refactor: move validation rules to a new file

--- ecommerce-application/src/widgets/RegistrationForm/lib/ValidationSchema.ts
+++ ecommerce-application/src/widgets/RegistrationForm/lib/ValidationSchema.ts
@@ -1,51 +1,23 @@
 import * as yup from 'yup';
 
-const regExpEmail = /^\S+@\S+\.\S+$/;
-const regExpName = /^[a-zA-Z]+$/;
-const minAge = 13 * 365 * 24 * 3600 * 1000;
-const maxDateofBirth = new Date(Date.now() - minAge);
-const regExpPostalCode = /^\d{5}$/;
+import { validationRules } from './ValidationRules';
 
 export const schema = yup.object().shape({
-  firstName: yup.string().trim().required('Field is required').matches(regExpName, 'No special characters/numbers'),
+  firstName: validationRules.namesAndCity,
+  lastName: validationRules.namesAndCity,
+  email: validationRules.email,
+  password: validationRules.password,
+  dateOfBirth: validationRules.dateOfBirth,
 
-  lastName: yup.string().trim().required('Field is required').matches(regExpName, 'No special characters/numbers'),
+  shippingStreet: validationRules.streetAndCountry,
+  billingStreet: validationRules.streetAndCountry,
 
-  email: yup.string().required('Field is required').matches(regExpEmail, 'Invalid email format'),
+  shippingCity: validationRules.namesAndCity,
+  billingCity: validationRules.namesAndCity,
 
-  password: yup
-    .string()
-    .required('Field is required')
-    .matches(/^[^\s]/, 'Password cannot start with a space')
-    .min(8, 'Enter at least 8 characters')
-    .matches(/\d/, 'Use at least one digit')
-    .matches(/[A-Z]/, 'Use at least one uppercase letter')
-    .matches(/[a-z]/, 'Use at least one lowercase letter')
-    .matches(/[^\s]$/, 'Password cannot end with a space'),
+  shippingPostalCode: validationRules.postalCode,
+  billingPostalCode: validationRules.postalCode,
 
-  dateOfBirth: yup
-    .date()
-    .typeError('Invalid date')
-    .required('Field is required')
-    .max(maxDateofBirth, 'You should be older than 13'),
-
-  shippingStreet: yup.string().trim().required('Field is required'),
-  billingStreet: yup.string().trim().required('Field is required'),
-
-  shippingCity: yup.string().trim().required('Field is required').matches(regExpName, 'No special characters/numbers'),
-  billingCity: yup.string().trim().required('Field is required').matches(regExpName, 'No special characters/numbers'),
-
-  shippingPostalCode: yup
-    .string()
-    .trim()
-    .required('Field is required')
-    .matches(regExpPostalCode, 'Should consist of 5 numbers'),
-  billingPostalCode: yup
-    .string()
-    .trim()
-    .required('Field is required')
-    .matches(regExpPostalCode, 'Should consist of 5 numbers'),
-
-  shippingCountry: yup.string().required('Field is required'),
-  billingCountry: yup.string().required('Field is required'),
+  shippingCountry: validationRules.streetAndCountry,
+  billingCountry: validationRules.streetAndCountry,
 });
