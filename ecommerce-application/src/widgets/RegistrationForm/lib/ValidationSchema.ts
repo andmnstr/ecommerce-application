754c1f3133c0fe077929068d30c8a8e5ae727555
adamanta1 <elena.brashkina2020@gmail.com>
2024-05-15 01:43:09 +0400

    feat: add validation rules for postal codes and countreis

--- ecommerce-application/src/widgets/RegistrationForm/lib/ValidationSchema.ts
+++ ecommerce-application/src/widgets/RegistrationForm/lib/ValidationSchema.ts
@@ -4,6 +4,7 @@ const regExpEmail = /^\S+@\S+\.\S+$/;
 const regExpName = /^[a-zA-Z]+$/;
 const minAge = 13 * 365 * 24 * 3600 * 1000;
 const maxDateofBirth = new Date(Date.now() - minAge);
+const regExpPostalCode = /^\d{5}$/;
 
 export const schema = yup.object().shape({
   firstName: yup.string().trim().required('Field is required').matches(regExpName, 'No special characters/numbers'),
@@ -33,4 +34,18 @@ export const schema = yup.object().shape({
 
   shippingCity: yup.string().trim().required('Field is required').matches(regExpName, 'No special characters/numbers'),
   billingCity: yup.string().trim().required('Field is required').matches(regExpName, 'No special characters/numbers'),
+
+  shippingPostalCode: yup
+    .string()
+    .trim()
+    .required('Field is required')
+    .matches(regExpPostalCode, 'Should consist of 5 numbers'),
+  billingPostalCode: yup
+    .string()
+    .trim()
+    .required('Field is required')
+    .matches(regExpPostalCode, 'Should consist of 5 numbers'),
+
+  shippingCountry: yup.string().required('Field is required'),
+  billingCountry: yup.string().required('Field is required'),
 });
