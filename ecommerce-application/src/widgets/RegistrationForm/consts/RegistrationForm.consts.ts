f4d96106ad7cd3a1d4c91a2f2a25e1c0d296091b
adamanta1 <elena.brashkina2020@gmail.com>
2024-05-18 22:27:34 +0400

    fix: add a type for dateView

--- ecommerce-application/src/widgets/RegistrationForm/consts/RegistrationForm.consts.ts
+++ ecommerce-application/src/widgets/RegistrationForm/consts/RegistrationForm.consts.ts
@@ -1,7 +1,9 @@
+import type { DateView } from '@mui/x-date-pickers';
+
 import type { InputNames } from '../lib/types/InputNamesType';
 
 export const countries = ['Finland', 'France', 'Germany', 'Italy', 'Serbia'];
-export const dateView = ['day', 'month', 'year'];
+export const dateView: DateView[] = ['day', 'month', 'year'];
 export const addressBoxLabels = ['Street', 'City', 'Postal Code', 'Country'];
 export const addressBoxInputs = [0, 1, 2];
 export const shippingAddressInputNames: InputNames[] = [
