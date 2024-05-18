df366be1076b2bcdb7e4d65151bcc7e9bbcb4eba
Andrey Monastyrev <117003637+andmnstr@users.noreply.github.com>
2024-05-18 20:07:06 +0500

    Feat: RSS_ECOMM_2_15. Signup Page Design Implementation (#86)

--- ecommerce-application/src/pages/SignupPage/ui/SignupPage.tsx
+++ ecommerce-application/src/pages/SignupPage/ui/SignupPage.tsx
@@ -1,5 +1,19 @@
 import type React from 'react';
 
+import { Header } from '../../../widgets/Header';
+import { RegistrationForm } from '../../../widgets/RegistrationForm';
+import styles from './SignupPage.module.scss';
+
 export const SignupPage: React.FC = () => {
-  return <div />;
+  return (
+    <>
+      <Header />
+      <div className={styles.SignupPage__container}>
+        <div className={styles.SignupPage__image} />
+        <div className={styles.SignupPage__inputs}>
+          <RegistrationForm />
+        </div>
+      </div>
+    </>
+  );
 };
