c3d5d3debf541cb35ce6a0c4dddbf05e0085bdb7
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-05-19 10:21:40 +0300

    feat: add enum with error messages

---
+++ ecommerce-application/src/widgets/LoginForm/Lib/errorMessages.ts
@@ -0,0 +1,4 @@
+export enum ErrorMessages {
+  WrongLoginOrPasswordError = 'Login failed. Review the email address and password entered and try again.',
+  OtherError = 'Unknown error. Please try again later.',
+}
