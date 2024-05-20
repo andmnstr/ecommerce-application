fc08baca8a31d9651ac76de2bf665e1d52397e4b
Andrey Monastyrev <117003637+andmnstr@users.noreply.github.com>
2024-05-21 01:41:42 +0500

    feat: add 'dummy' text & headers to each page (#96)

--- ecommerce-application/src/pages/ShopPage/ui/ShopPage.tsx
+++ ecommerce-application/src/pages/ShopPage/ui/ShopPage.tsx
@@ -1,5 +1,16 @@
 import type React from 'react';
 
+import { Header } from '../../../widgets/Header';
+import styles from './ShopPage.module.scss';
+
 export const ShopPage: React.FC = () => {
-  return <div />;
+  return (
+    <div>
+      <Header />
+      <div className={styles.content}>
+        <h2 className={styles.text_heading}>Oops! :(</h2>
+        <h2 className={styles.text}>Content is not ready at this moment...</h2>
+      </div>
+    </div>
+  );
 };
