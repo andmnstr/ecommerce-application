d6d8d8d699a13646aec2b21350e06f476d38a301
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-05-23 23:13:30 +0400

    feat: save apiRoot after application start

--- ecommerce-application/src/app/App.tsx
+++ ecommerce-application/src/app/App.tsx
@@ -1,9 +1,9 @@
 import type React from 'react';
 
-import { gettingToken } from '../shared/Api';
+import { gettingToken, setApiRoot } from '../shared/Api';
 import { PageRoutes } from './providers/router/PageRoutes';
 
 export const App: React.FC = () => {
-  gettingToken();
+  setApiRoot(gettingToken());
   return <PageRoutes />;
 };
