ade1d6adbdf51ae0a2666af4339c966d888658f3
adamanta1 <elena.brashkina2020@gmail.com>
2024-05-25 01:16:31 +0400

    feat: add styles for UserProfile widget, add defauld addresses titles and button to add new address

--- ecommerce-application/src/widgets/UserProfile/UI/UserProfile.tsx
+++ ecommerce-application/src/widgets/UserProfile/UI/UserProfile.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 import { CustomButton } from '../../../shared/UI/button/CustomButton';
 import CustomInputText from '../../../shared/UI/CustomInputText/CustomInputText';
+import classes from './UserProfile.module.scss';
 
 export const UserProfile: React.FC = () => {
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
@@ -14,42 +15,64 @@ export const UserProfile: React.FC = () => {
 
   return (
     <>
-      <Typography component="h1">My profile</Typography>
+      <Typography
+        component="h1"
+        className={classes.Title}
+      >
+        My profile
+      </Typography>
       <Tabs
         value={currentTabIndex}
         onChange={handleTabChange}
       >
-        <Tab label="Personal informaton" />
-        <Tab label="Addresses" />
-        <Tab label="Password manager" />
+        <Tab
+          label="Personal informaton"
+          className={classes.Tab}
+        />
+        <Tab
+          label="Addresses"
+          className={classes.Tab}
+        />
+        <Tab
+          label="Password manager"
+          className={classes.Tab}
+        />
       </Tabs>
 
       {currentTabIndex === 0 && (
-        <Box>
+        <Box
+          component="form"
+          className={classes.Container}
+        >
           <CustomInputText
             name="firstName"
             label="First Name"
+            className={classes.Input}
             disabled
           />
           <CustomInputText
             name="lastName"
             label="Last Name"
+            className={classes.Input}
             disabled
           />
           <CustomInputText
             name="dateOfBirth"
             label="Date of Birth"
+            className={classes.Input}
             disabled
           />
           <CustomInputText
             name="email"
             label="Email"
+            className={classes.Input}
             disabled
           />
           <CustomButton
             variant="contained"
             size="large"
             type="button"
+            className={classes.Button}
           >
             Edit
           </CustomButton>
@@ -57,8 +80,9 @@ export const UserProfile: React.FC = () => {
       )}
 
       {currentTabIndex === 1 && (
-        <Box>
-          <Box>
+        <Box className={classes.Container}>
+          <Typography component="h2">Default shipping address</Typography>
+          <Box className={classes.AddressContainer}>
             <CustomInputText label="Street" />
             <CustomInputText label="City" />
             <CustomInputText label="Postal Code" />
@@ -66,20 +90,22 @@ export const UserProfile: React.FC = () => {
             <CustomButton
               variant="contained"
               size="large"
-              type="submit"
+              type="button"
+              className={classes.Button}
             >
               Edit
             </CustomButton>
             <CustomButton
               variant="contained"
               size="large"
-              type="submit"
+              type="button"
+              className={classes.Button}
             >
               Delete
             </CustomButton>
           </Box>
-
-          <Box>
+          <Typography component="h2">Default billing address</Typography>
+          <Box className={classes.AddressContainer}>
             <CustomInputText label="Street" />
             <CustomInputText label="City" />
             <CustomInputText label="Postal Code" />
@@ -87,23 +113,34 @@ export const UserProfile: React.FC = () => {
             <CustomButton
               variant="contained"
               size="large"
-              type="submit"
+              type="button"
+              className={classes.Button}
             >
               Edit
             </CustomButton>
             <CustomButton
               variant="contained"
               size="large"
-              type="submit"
+              type="button"
+              className={classes.Button}
             >
               Delete
             </CustomButton>
           </Box>
+          <CustomButton
+            variant="contained"
+            size="large"
+            type="button"
+            className={classes.Button}
+            style={{ width: 300, alignSelf: 'center', marginTop: 20 }}
+          >
+            Add new address
+          </CustomButton>
         </Box>
       )}
 
       {currentTabIndex === 2 && (
-        <Box>
+        <Box className={classes.Container}>
           <CustomInputText
             label="Current password"
             fullWidth
@@ -116,13 +153,15 @@ export const UserProfile: React.FC = () => {
             variant="contained"
             size="large"
             type="submit"
+            className={classes.Button}
           >
             Save
           </CustomButton>
           <CustomButton
             variant="contained"
             size="large"
-            type="submit"
+            type="button"
+            className={classes.Button}
           >
             Cancel
           </CustomButton>
