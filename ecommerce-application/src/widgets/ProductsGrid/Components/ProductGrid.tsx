2ec5c406235c3c150ca69052d717e5717a067d78
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-05-24 23:51:35 +0300

    feat: add product grid component

---
+++ ecommerce-application/src/widgets/ProductsGrid/Components/ProductGrid.tsx
@@ -0,0 +1,60 @@
+import type { ProductProjection } from '@commercetools/platform-sdk';
+import { Grid } from '@mui/material';
+import type React from 'react';
+import { useEffect, useState } from 'react';
+
+import { ProductCard } from '../../../entities';
+import { getApiRoot } from '../../../shared/Api/apiRoot';
+import { fetchProducts } from '../Api/fetchProducts';
+import classes from './ProductionGrid.module.scss';
+
+export const ProductGrid: React.FC = () => {
+  const [products, setProducts] = useState<ProductProjection[]>([]);
+  useEffect(() => {
+    fetchProducts(getApiRoot()).then(response => {
+      setProducts(response);
+    });
+  }, []);
+  return (
+    <Grid
+      container
+      spacing={2}
+      className={classes.grid}
+    >
+      {products.map(product => {
+        const productVariants = product.variants[0];
+        let key = '';
+        let name = '';
+        let description = '';
+        let image = '';
+        let price = '';
+        if (productVariants.images && productVariants.images.length) {
+          image = productVariants.images[0].url;
+        }
+        key = product.id;
+        name = product.name['ru-RU'];
+        if (product.description) {
+          description = product.description['ru-RU'];
+        }
+        if (productVariants.prices && productVariants.prices.length) {
+          price = `€${productVariants.prices[0].value.centAmount}`;
+        }
+        return (
+          <Grid
+            item
+            key={key}
+            className={classes.gridItem}
+          >
+            <ProductCard
+              key={key}
+              name={name}
+              image={image}
+              description={description}
+              price={price}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
