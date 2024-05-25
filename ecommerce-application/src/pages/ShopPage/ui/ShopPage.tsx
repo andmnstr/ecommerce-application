2f27c1920ff45e650d43cc79bbb5bea646220622
Baltika-Cat <134006853+Baltika-Cat@users.noreply.github.com>
2024-05-25 07:23:34 +0400

    Feat: RSS_ECOMM_3_5. Product List Implementation (#100)
    * feat: implement function for getting of prduct's data
    
    * feat: add interface for product's data
    
    * feat: add product card component
    
    * feat: add styles for product card
    
    * feat: add product grid component
    
    * feat: add styles for product grid
    
    * feat: add product grid to shop page
    
    * feat: add re-exports through 'index' files
    
    * fix: make show of price correct
    
    * refactor: change path to product's data

--- ecommerce-application/src/pages/ShopPage/ui/ShopPage.tsx
+++ ecommerce-application/src/pages/ShopPage/ui/ShopPage.tsx
@@ -1,16 +1,13 @@
 import type React from 'react';
 
+import { ProductGrid } from '../../../widgets';
 import { Header } from '../../../widgets/Header';
-import styles from './ShopPage.module.scss';
 
 export const ShopPage: React.FC = () => {
   return (
     <div>
       <Header />
-      <div className={styles.content}>
-        <h2 className={styles.text_heading}>Oops! :(</h2>
-        <h2 className={styles.text}>Content is not ready at this moment...</h2>
-      </div>
+      <ProductGrid />
     </div>
   );
 };
---
+++ ecommerce-application/src/widgets/ProductsGrid/Components/ProductGrid.tsx
@@ -0,0 +1,64 @@
+import type { ProductProjection } from '@commercetools/platform-sdk';
+import { Grid } from '@mui/material';
+import type React from 'react';
+import { useEffect, useState } from 'react';
+
+import { ProductCard } from '../../../entities';
+import { getApiRoot } from '../../../shared/Api/apiRoot';
+import { fetchProducts } from '../Api/fetchProducts';
+import classes from './ProductGrid.module.scss';
+
+export const ProductGrid: React.FC = () => {
+  const [products, setProducts] = useState<ProductProjection[]>([]);
+  useEffect(() => {
+    fetchProducts(getApiRoot()).then(response => {
+      setProducts(response);
+    });
+  }, []);
+  return (
+    <Grid
+      container
+      spacing={2}
+      className={classes.grid}
+    >
+      {products.map(product => {
+        const productVariants = product.masterVariant;
+        const centsPerEuro = 100;
+        let key = '';
+        let name = '';
+        let description = '';
+        let image = '';
+        let price = '';
+        if (productVariants.images && productVariants.images.length) {
+          image = productVariants.images[0].url;
+        }
+        key = product.id;
+        name = product.name['ru-RU'];
+        if (product.description) {
+          description = product.description['ru-RU'];
+        }
+        if (productVariants.prices && productVariants.prices.length) {
+          price = (productVariants.prices[0].value.centAmount / centsPerEuro).toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'EUR',
+          });
+        }
+        return (
+          <Grid
+            item
+            key={key}
+            className={classes.gridItem}
+          >
+            <ProductCard
+              key={key}
+              name={name}
+              image={image}
+              description={description}
+              price={price}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
