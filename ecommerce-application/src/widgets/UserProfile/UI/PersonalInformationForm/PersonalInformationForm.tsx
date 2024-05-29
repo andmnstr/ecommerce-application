6b80ce5142f96d04c8bd9b26567b8717e80d302c
adamanta1 <elena.brashkina2020@gmail.com>
2024-05-30 02:35:05 +0400

    feat: add a request to the server to change presonal information

--- ecommerce-application/src/widgets/UserProfile/UI/PersonalInformationForm/PersonalInformationForm.tsx
+++ ecommerce-application/src/widgets/UserProfile/UI/PersonalInformationForm/PersonalInformationForm.tsx
@@ -8,19 +8,36 @@ import type React from 'react';
 import { useState } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
+import { getApiRoot } from '../../../../shared';
 import { CustomButton } from '../../../../shared/UI/button/CustomButton';
 import { dateView } from '../../../RegistrationForm/consts/RegistrationForm.consts';
-import type { IRegistrationFields } from '../../../RegistrationForm/lib/types/RegistrationFields';
-import { schema } from '../../../RegistrationForm/lib/ValidationSchema';
+import { validationRules } from '../../../RegistrationForm/lib/ValidationRules';
+// import type { IRegistrationFields } from '../../../RegistrationForm/lib/types/RegistrationFields';
+// import { schema } from '../../../RegistrationForm/lib/ValidationSchema';
 import classes from '../UserProfile.module.scss';
 
 interface IPersonalInformationFormProps {
   userInfo: Customer;
 }
 
+export interface IRegistrationFields {
+  firstName: string;
+  lastName: string;
+  email: string;
+  dateOfBirth: Date;
+}
+
+export const schema = yup.object().shape({
+  firstName: validationRules.namesAndCity,
+  lastName: validationRules.namesAndCity,
+  email: validationRules.email,
+  dateOfBirth: validationRules.dateOfBirth,
+});
+
 export const PersonalInformationForm: React.FC<IPersonalInformationFormProps> = ({ userInfo }) => {
-  const { firstName, lastName, dateOfBirth, email } = userInfo;
+  const { firstName, lastName, dateOfBirth, email, version } = userInfo;
 
   const {
     handleSubmit,
@@ -47,8 +64,38 @@ export const PersonalInformationForm: React.FC<IPersonalInformationFormProps> = 
     setSavetButtonClass(classes.Button);
   };
 
-  const submitForm: SubmitHandler<IRegistrationFields> = () => {
-    console.log('meow');
+  const submitForm: SubmitHandler<IRegistrationFields> = async formData => {
+    const date = new Date(formData.dateOfBirth);
+    const correstDateOfBirth = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+    const response = await getApiRoot()
+      .me()
+      .post({
+        body: {
+          version,
+          actions: [
+            {
+              action: 'setFirstName',
+              firstName: formData.firstName,
+            },
+            {
+              action: 'setLastName',
+              lastName: formData.lastName,
+            },
+            {
+              action: 'setDateOfBirth',
+              dateOfBirth: correstDateOfBirth,
+            },
+            {
+              action: 'changeEmail',
+              email: formData.email,
+            },
+          ],
+        },
+      })
+      .execute();
+
+    console.log('Customer updated:', response);
     setIsDisabled(true);
     setEditButtonClass(classes.Button);
     setSavetButtonClass(classes.Invisible);
