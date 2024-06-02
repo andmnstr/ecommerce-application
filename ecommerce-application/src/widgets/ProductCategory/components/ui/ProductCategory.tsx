07bd54c7c3df8db5f31b4a7137a5b5b2df01f9e2
Andrey Monastyrev <117003637+andmnstr@users.noreply.github.com>
2024-06-02 23:12:51 +0500

    Feat: RSS_ECOMM_3_4. Product Categories Implementation (#110)
    * feat: add 'category section' with 'skeleton' component
    
    * feat: add 'ProductCategory' widget for layout categories in 'shop' page
    
    * feat: implement functionality for getting all products categories & subcategories
    
    * feat: implement layout of category cards on a 'shop' page
    
    * feat: implement product categories & subcategories on a 'shop' page

---
+++ ecommerce-application/src/widgets/ProductCategory/components/ui/ProductCategory.tsx
@@ -0,0 +1,52 @@
+import { Card, Divider, List, ListItem } from '@mui/material';
+import type React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './ProductCategory.module.scss';
+
+interface IProductCategoryProps {
+  category: string | undefined;
+  subcategory: string[] | undefined;
+  imageURL: string | undefined;
+}
+
+export const ProductCategory: React.FC<IProductCategoryProps> = props => {
+  const { category, subcategory, imageURL } = props;
+  const subcatArr: string[] = [];
+
+  if (subcategory !== undefined && subcategory.length > 0) {
+    subcategory.forEach(subcategoryItem => {
+      subcatArr.push(subcategoryItem);
+    });
+  }
+
+  return (
+    <Card className={styles.category}>
+      <div
+        className={styles.category_card}
+        style={{ backgroundImage: `url(${imageURL})` }}
+      />
+      <Link
+        to={`/shop/${category}`}
+        className={styles.category_name}
+      >
+        {category ?? ''}
+      </Link>
+      <Divider style={{ width: '90%', margin: 'auto' }} />
+      <List>
+        {subcatArr.map(item => {
+          return (
+            <ListItem>
+              <Link
+                to={`/shop/${category}/${item}`}
+                className={styles.subcategory_name}
+              >
+                {item}
+              </Link>
+            </ListItem>
+          );
+        })}
+      </List>
+    </Card>
+  );
+};
