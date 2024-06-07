aba5d8b64fc7d6eb164e941967ba9b7c85639e9d
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-06-07 19:58:54 +0400

    feat: add pagination for products

--- ecommerce-application/src/widgets/ProductsGrid/Components/ProductGrid.tsx
+++ ecommerce-application/src/widgets/ProductsGrid/Components/ProductGrid.tsx
@@ -1,5 +1,6 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Pagination, Stack, Typography } from '@mui/material';
 import type React from 'react';
+import { useEffect, useState } from 'react';
 
 import { ProductCard } from '../../../entities';
 import { isColorValue } from '../Lib/predicates';
@@ -7,7 +8,11 @@ import type { IProducts, IVariants } from '../Lib/types';
 import classes from './ProductGrid.module.scss';
 import { ProductSearch } from './ProductSearch/ProductSearch';
 
-export const ProductGrid: React.FC<IProducts> = ({ products, action, sort, onSearch }) => {
+export const ProductGrid: React.FC<IProducts> = ({ products, action, sort, onSearch, toStartPage }) => {
+  const [pageCount, setPageCount] = useState<number>(0);
+  const [productsOnPage, setProductsOnPage] = useState<IVariants[]>();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const productsQuantityPerPage = 30;
   const discountPercent = 20;
   const centsPerEuro = 100;
   const colors: string[] = [];
@@ -28,11 +33,49 @@ export const ProductGrid: React.FC<IProducts> = ({ products, action, sort, onSea
     });
     return acc;
   }, []);
+  useEffect(() => {
+    const newPageCount = Math.ceil(variants.length / productsQuantityPerPage);
+    if (pageCount !== newPageCount) {
+      setPageCount(newPageCount);
+    }
+
+    const newProductsOnPage = variants.slice(
+      (currentPage - 1) * productsQuantityPerPage,
+      currentPage * productsQuantityPerPage
+    );
+    if (JSON.stringify(productsOnPage) !== JSON.stringify(newProductsOnPage)) {
+      setProductsOnPage(newProductsOnPage);
+    }
+  }, [currentPage, variants, pageCount, productsOnPage, productsQuantityPerPage]);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [toStartPage]);
   if (action === 'filter') {
     variants = variants.filter(variant => {
       return variant.variant.isMatchingVariant;
     });
   }
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number): void => {
+    setCurrentPage(value);
+  };
+  variants = variants.filter(item => {
+    const { variant } = item;
+    if (variant.attributes) {
+      const color = variant.attributes.find(attribute => {
+        return attribute.name === 'color';
+      });
+      if (
+        color &&
+        isColorValue(color.value) &&
+        !colors.includes(JSON.stringify({ id: item.productId, color: color.value['ru-RU'] }))
+      ) {
+        colors.push(JSON.stringify({ id: item.productId, color: color.value['ru-RU'] }));
+        return true;
+      }
+    }
+    return false;
+  });
   if (sort === 'ascending' || sort === 'descending') {
     variants.sort((a, b) => {
       if (a.variant.prices && b.variant.prices) {
@@ -48,87 +91,101 @@ export const ProductGrid: React.FC<IProducts> = ({ products, action, sort, onSea
       <Box className={classes.searchContainer}>
         <ProductSearch onSubmit={onSearch} />
       </Box>
-      {variants.length ? (
-        <Grid
-          container
-          spacing={2}
-          className={classes.grid}
-        >
-          {variants.map(currentVariant => {
-            const { variant, productId, productDescription, productKey, productCategories, productName } =
-              currentVariant;
-            if (variant.attributes) {
-              const color = variant.attributes.find(item => {
-                return item.name === 'color';
-              });
-              if (
-                color &&
-                isColorValue(color.value) &&
-                !colors.includes(JSON.stringify({ id: productId, color: color.value['ru-RU'] }))
-              ) {
-                colors.push(JSON.stringify({ id: productId, color: color.value['ru-RU'] }));
-                let id = '';
-                let key = '';
-                let name = '';
-                let description = '';
-                let image = '';
-                let price = '';
-                let category = '';
-                let productLink = '';
-                let oldPrice = '';
-                if (variant.images && variant.images.length) {
-                  image = variant.images[0].url;
-                }
-                id = productId;
-                key = `${productId}-${variant.id}`;
-                name = productName['ru-RU'];
-                description = productDescription['ru-RU'];
-                if (productKey) {
-                  productLink = productKey;
-                }
-                if (productCategories[0]) {
-                  category = productCategories[0].id;
-                }
-                if (variant.prices && variant.prices.length) {
-                  oldPrice = (variant.prices[0].value.centAmount / centsPerEuro).toLocaleString('en-US', {
+      {productsOnPage && productsOnPage.length ? (
+        <Box className={classes.gridContainer}>
+          <Grid
+            container
+            spacing={2}
+            className={classes.grid}
+          >
+            {productsOnPage.map(currentVariant => {
+              const { variant, productId, productDescription, productKey, productCategories, productName } =
+                currentVariant;
+              let id = '';
+              let key = '';
+              let name = '';
+              let description = '';
+              let image = '';
+              let price = '';
+              let category = '';
+              let productLink = '';
+              let oldPrice = '';
+              if (variant.images && variant.images.length) {
+                image = variant.images[0].url;
+              }
+              id = productId;
+              key = `${productId}-${variant.id}`;
+              name = productName['ru-RU'];
+              description = productDescription['ru-RU'];
+              if (productKey) {
+                productLink = productKey;
+              }
+              if (productCategories[0]) {
+                category = productCategories[0].id;
+              }
+              if (variant.prices && variant.prices.length) {
+                oldPrice = (variant.prices[0].value.centAmount / centsPerEuro).toLocaleString('en-US', {
+                  style: 'currency',
+                  currency: 'EUR',
+                });
+                if (variant.prices[0].discounted) {
+                  price = (variant.prices[0].discounted.value.centAmount / centsPerEuro).toLocaleString('en-US', {
                     style: 'currency',
                     currency: 'EUR',
                   });
-                  if (variant.prices[0].discounted) {
-                    price = (variant.prices[0].discounted.value.centAmount / centsPerEuro).toLocaleString('en-US', {
-                      style: 'currency',
-                      currency: 'EUR',
-                    });
-                  } else {
-                    const oldPriceNumber = +oldPrice.slice(1);
-                    price = `€${(oldPriceNumber - oldPriceNumber * (discountPercent / 100)).toFixed(2)}`;
-                  }
+                } else {
+                  const oldPriceNumber = +oldPrice.slice(1);
+                  price = `€${(oldPriceNumber - oldPriceNumber * (discountPercent / 100)).toFixed(2)}`;
                 }
-                return (
-                  <Grid
-                    item
-                    key={key}
-                    className={classes.gridItem}
-                  >
-                    <ProductCard
-                      id={id}
-                      key={key}
-                      name={name}
-                      image={image}
-                      description={description}
-                      price={price}
-                      product={key}
-                      category={category}
-                      productLink={productLink}
-                      oldPrice={oldPrice}
-                    />
-                  </Grid>
-                );
               }
-            }
-            return null;
-          })}
-        </Grid>
+              return (
+                <Grid
+                  item
+                  key={key}
+                  className={classes.gridItem}
+                >
+                  <ProductCard
+                    id={id}
+                    key={key}
+                    name={name}
+                    image={image}
+                    description={description}
+                    price={price}
+                    product={key}
+                    category={category}
+                    productLink={productLink}
+                    oldPrice={oldPrice}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+          <Stack
+            spacing={2}
+            className={classes.pagination}
+          >
+            <Pagination
+              className={classes.paginationElement}
+              count={pageCount}
+              variant="outlined"
+              shape="rounded"
+              page={currentPage}
+              onChange={handleChange}
+              sx={{
+                '& .MuiButtonBase-root': {
+                  backgroundColor: '#222',
+                  color: 'white',
+                },
+                '& .Mui-selected': {
+                  backgroundColor: '#555',
+                },
+                '& .Mui-selected:hover': {
+                  backgroundColor: '#555',
+                },
+              }}
+            />
+          </Stack>
+        </Box>
       ) : (
         <Box className={classes.noMatchTextContainer}>
           <Typography className={classes.noMatchText}>Sorry, no matches found</Typography>
