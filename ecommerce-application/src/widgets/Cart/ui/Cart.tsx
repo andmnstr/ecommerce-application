11b21e832838369619bb98e755d5bc5ba02781a4
adamanta1 <elena.brashkina2020@gmail.com>
2024-06-10 21:26:09 +0400

    feat: make adaptive design for cart

--- ecommerce-application/src/widgets/Cart/ui/Cart.tsx
+++ ecommerce-application/src/widgets/Cart/ui/Cart.tsx
@@ -1,6 +1,9 @@
 import { Add, DeleteForever, Remove } from '@mui/icons-material';
 import {
   Box,
+  Divider,
+  List,
+  ListItem,
   Stack,
   Table,
   TableBody,
@@ -9,6 +12,7 @@ import {
   TableHead,
   TableRow,
   Typography,
+  useMediaQuery,
 } from '@mui/material';
 import type React from 'react';
 
@@ -17,49 +21,85 @@ import { CustomButton } from '../../../shared/UI/button/CustomButton';
 import classes from './Cart.module.scss';
 
 export const Cart: React.FC = () => {
+  const smallScreen = useMediaQuery('(max-width: 767px)');
   return (
     <Box className={classes.CartContainer}>
-      <TableContainer className={classes.CartProductsTable}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Products</TableCell>
-              <TableCell>Price</TableCell>
-              <TableCell>Quantity</TableCell>
-              <TableCell>Total</TableCell>
-              <TableCell />
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow key="0">
-              <TableCell className={classes.ProductsCell}>
-                <Box
-                  component="img"
-                  src={KittenImage}
-                  className={classes.CartItemImage}
-                />
-                <Stack>
-                  <Typography sx={{ fontWeight: 700 }}>Kitten Kitten Kitten Kitten</Typography>
-                  <Typography>Size: S</Typography>
-                </Stack>
-              </TableCell>
-              <TableCell>$80.00</TableCell>
-              <TableCell>
+      {!smallScreen && (
+        <TableContainer className={classes.CartProductsTable}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Products</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Quantity</TableCell>
+                <TableCell>Total</TableCell>
+                <TableCell />
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow key="0">
+                <TableCell className={classes.ProductsCell}>
+                  <Box
+                    component="img"
+                    src={KittenImage}
+                    className={classes.CartItemImage}
+                  />
+                  <Stack>
+                    <Typography sx={{ fontWeight: 700 }}>Kitten Kitten Kitten Kitten</Typography>
+                    <Typography>Size: S</Typography>
+                  </Stack>
+                </TableCell>
+                <TableCell>$80.00</TableCell>
+                <TableCell>
+                  <Stack className={classes.ItemQuantity}>
+                    <Remove />
+                    <Typography>1</Typography>
+                    <Add />
+                  </Stack>
+                </TableCell>
+                <TableCell>$80.00</TableCell>
+                <TableCell size="small">
+                  <DeleteForever />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+      {smallScreen && (
+        <List className={classes.CartProductsList}>
+          <Divider
+            variant="fullWidth"
+            component="li"
+          />
+          <ListItem className={classes.CartListItem}>
+            <Box
+              component="img"
+              src={KittenImage}
+              className={classes.CartItemImage}
+            />
+            <Stack className={classes.CartListItemDetails}>
+              <Typography sx={{ fontWeight: 700 }}>Kitten Kitten Kitten Kitten</Typography>
+              <Typography>Size: S</Typography>
+              <Stack className={classes.ItemQuantityAndPrice}>
                 <Stack className={classes.ItemQuantity}>
                   <Remove />
                   <Typography>1</Typography>
                   <Add />
                 </Stack>
-              </TableCell>
-              <TableCell>$80.00</TableCell>
-              <TableCell size="small">
-                <DeleteForever />
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-
+                <Typography sx={{ fontWeight: 700 }}>$80.00</Typography>
+              </Stack>
+            </Stack>
+            <Box className={classes.DeleteButton}>
+              <DeleteForever fontSize="large" />
+            </Box>
+          </ListItem>
+          <Divider
+            variant="fullWidth"
+            component="li"
+          />
+        </List>
+      )}
       <Box className={classes.TotalPriceBox}>
         <Stack className={classes.TotalPrice}>
           <Typography sx={{ fontWeight: 700 }}>Grand Total:</Typography>
