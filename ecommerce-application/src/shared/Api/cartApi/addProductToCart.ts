106ca3b74d04b2587c0db93a0efd782ebc83cb21
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-06-11 17:13:17 +0400

    feat: add request for addition product to cart

---
+++ ecommerce-application/src/shared/Api/cartApi/addProductToCart.ts
@@ -0,0 +1,52 @@
+import { CART_ID } from '../../consts/cartId';
+import { DISTRIBUTION_CHANNEL } from '../../consts/distributionChannel';
+import { getApiRoot } from '../apiRoot';
+import { createProductCart } from './createProductCart';
+import { getExistingProductCart } from './getExistingProductCart';
+
+export const addProductToCart = async (sku: string): Promise<void> => {
+  let version = 0;
+  let cartId = '';
+  const id = localStorage.getItem(CART_ID);
+  if (id && typeof id === 'string') {
+    console.log(true);
+    cartId = id;
+  }
+  try {
+    const cart = await getExistingProductCart();
+    version = cart.body.version;
+    cartId = cart.body.id;
+    localStorage.setItem(CART_ID, cart.body.id);
+  } catch {
+    const cart = await createProductCart();
+    version = cart.body.version;
+    cartId = cart.body.id;
+  } finally {
+    console.log('CARTID', cartId);
+    await getApiRoot()
+      .me()
+      .carts()
+      .withId({ ID: cartId })
+      .post({
+        body: {
+          version,
+          actions: [
+            {
+              action: 'setCountry',
+              country: 'EU',
+            },
+            {
+              action: 'addLineItem',
+              sku,
+              quantity: 1,
+              distributionChannel: {
+                typeId: 'channel',
+                id: DISTRIBUTION_CHANNEL,
+              },
+            },
+          ],
+        },
+      })
+      .execute();
+  }
+};
