0383f47f90cef45b9366d6a36b1540028c4aa0d0
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-06-11 17:20:16 +0400

    refactor: add re-exports of api-requests through 'index.ts' files

--- ecommerce-application/src/shared/Api/index.ts
+++ ecommerce-application/src/shared/Api/index.ts
@@ -1,6 +1,7 @@
 export { setApiRoot } from './apiRoot';
 export { getApiRoot } from './apiRoot';
 export { authorizeCustomer, handleRequest } from './authorizationRequest';
+export { addProductToCart, createProductCart, getExistingProductCart } from './cartApi';
 export { createClientWitAnonimousToken } from './createClientWithAnonymousToken';
 export { createClientWithExistingToken } from './createClientWithExistingToken';
 export { createClientWithRefreshedToken } from './createClientWithRefreshedToken';
--- ecommerce-application/src/shared/index.ts
+++ ecommerce-application/src/shared/index.ts
@@ -1,13 +1,18 @@
 export {
+  addProductToCart,
   authorizeCustomer,
   createClientWitAnonimousToken,
   createClientWithRefreshedToken,
+  createProductCart,
   getApiRoot,
+  getExistingProductCart,
   gettingToken,
   handleRequest,
   setApiRoot,
 } from './Api';
+export { CART_ID } from './consts/cartId';
 export { colorTheme } from './consts/colorTheme';
 export { countries } from './consts/countries';
+export { DISTRIBUTION_CHANNEL } from './consts/distributionChannel';
 export { TOKEN_NAME } from './consts/tokenName';
 export { validationRules } from './consts/ValidationRules';
