f5ca09cfbf565b6e84f3ee0b1392a0312dbceae6
Baltika-Cat <134006853+Baltika-Cat@users.noreply.github.com>
2024-06-14 14:02:02 +0400

    Feat: RSS_ECOMM_4_17. Addition Cart Icon To Header (#145)
    * feat: add cart icon to navbar
    
    * feat: add cart icon to burger menu
    
    * feat: implement routing to cart page by cart icon on click or by entering url in browser address bar

--- ecommerce-application/src/widgets/Header/UI/Burger/config.ts
+++ ecommerce-application/src/widgets/Header/UI/Burger/config.ts
@@ -2,6 +2,7 @@ export const appPages = [
   { id: 1, text: 'Home', href: '/' },
   { id: 2, text: 'Shop', href: '/shop' },
   { id: 3, text: 'About us', href: '/about' },
+  { id: 4, text: 'Cart', href: '/cart' },
 ];
 
 export const authPages = [
--- ecommerce-application/src/widgets/Header/UI/Navbar/config.ts
+++ ecommerce-application/src/widgets/Header/UI/Navbar/config.ts
@@ -1,5 +1,8 @@
+import { ShoppingCart } from '@mui/icons-material';
+
 export const items = [
   { id: 1, text: 'Home', href: '/' },
   { id: 2, text: 'Shop', href: '/shop' },
   { id: 3, text: 'About us', href: '/about' },
+  { id: 4, text: ShoppingCart, href: '/cart' },
 ];
